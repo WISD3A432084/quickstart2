431667eafd4d923fcc9ccc507e106178b235f209
Xin <lovemarks19@gmail.com>
2017-10-22 22:10:39 +0800

    建立_新增、刪除、顯示_三個路由

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::auth();
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/tasks', 'TaskController@index');
+
+Route::post('/task', 'TaskController@store');
+
+Route::delete('/task/{task}', 'TaskController@destroy');
