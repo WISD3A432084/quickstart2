98a4ef04ebf5bedaf13d763143b33d8701a01e54
Xin <lovemarks19@gmail.com>
2017-10-24 23:03:21 +0800

    注入資源庫_TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,20 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
+    /**
+     * 任務資源庫的實例。
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
     /**
      * 建立一個新的控制器實例。
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+        $this->tasks = $tasks;
     }
 
     /**
@@ -26,7 +35,9 @@ class TaskController extends Controller
      */
     public function index(Request $request)
     {
-        return view('tasks.index');
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
     }
 
     /**
