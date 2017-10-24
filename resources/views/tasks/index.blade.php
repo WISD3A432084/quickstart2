1b086245a575bb3f3dac3bbdcddbe8448f4f0dc3
Xin <lovemarks19@gmail.com>
2017-10-24 23:04:41 +0800

    新增刪除按鈕

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -60,6 +60,15 @@
 
                             <td>
                                 <!-- 代辦：刪除按鈕 -->
+                                <!-- 刪除按鈕 -->
+                            <td>
+                                <form action="/task/{{ $task->id }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                    <button>刪除任務</button>
+                                </form>
+                            </td>
                             </td>
                         </tr>
                     @endforeach
