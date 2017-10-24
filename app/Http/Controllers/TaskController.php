98e1dff16fb5c18ed5e58d6a98a2467eb99922d3
Xin <lovemarks19@gmail.com>
2017-10-24 23:05:47 +0800

    定義 TaskController 的 destroy 方法

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -59,4 +59,16 @@ class TaskController extends Controller
 
         return redirect('/tasks');
     }
+
+    /**
+     * 移除給定的任務。
+     *
+     * @param  Request  $request
+     * @param  Task  $task
+     * @return Response
+     */
+    public function destroy(Request $request, Task $task)
+    {
+        //
+    }
 }
